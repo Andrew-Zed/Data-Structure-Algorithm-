3717017c10519e097d90e30a8c87b50f24e6541f
Andrew-Zed <andrew.awadyke@gmail.com>
2023-01-24 22:26:39 +0100

    array reversed using iterative solution

---
+++ src/com/andrew/bigOQustion/Reverse_Array.java
@@ -0,0 +1,24 @@
+package com.andrew.bigOQustion;
+
+import java.util.Arrays;
+
+public class Reverse_Array {
+
+    public static void main(String[] args) {
+        Reverse_Array reverseArray = new Reverse_Array();
+        int[] customArray = {1, 2, 3, 4, 5, 6, 7, 8};
+        reverseArray.reverse(customArray);
+
+    }
+
+    void reverse(int[] array) {
+      for (int i=0; i<array.length/2; i++){
+          int other = array.length-i - 1;
+          int temp = array[i];
+          array[i] = array[other];
+          array[other] = temp;
+      }
+        System.out.println(Arrays.toString(array));
+    }
+
+}
