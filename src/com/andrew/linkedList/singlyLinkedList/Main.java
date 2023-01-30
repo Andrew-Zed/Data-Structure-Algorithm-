541d33b9a0529c8272ef8f667cd76389856d0bba
Andrew-Zed <andrew.awadyke@gmail.com>
2023-01-30 23:47:09 +0100

    insertion into linked list implmented

--- src/com/andrew/linkedList/singlyLinkedList/Main.java
+++ src/com/andrew/linkedList/singlyLinkedList/Main.java
@@ -5,6 +5,10 @@ public class Main {
         Singly_LinkedList sLL = new Singly_LinkedList();
         sLL.createSinglyLinkedList(5);
         System.out.println(sLL.head.value);
+        sLL.insertInLinkedList(6, 1);
+        System.out.println(sLL.head.next.value);
+        sLL.insertInLinkedList(7, 3);
+        System.out.println(sLL.head.next.next.value);
     }
 
 }
