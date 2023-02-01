5b1a37c429e8bdd9b552e3357cd814be3d863f51
Andrew-Zed <andrew.awadyke@gmail.com>
2023-02-01 18:53:37 +0100

    insertion method Circular singly linkedList implemented

--- src/com/andrew/linkedList/circularSingleLinkedList/Main.java
+++ src/com/andrew/linkedList/circularSingleLinkedList/Main.java
@@ -4,7 +4,9 @@ public class Main {
     public static void main(String[] args) {
         CircularSinglyLinkedList csll = new CircularSinglyLinkedList();
         csll.createCSLL(5);
+        csll.insertCSLL(4,0); // inserting a node at the beginning of LinkedList (location 0)
+        csll.insertCSLL(7, 1); // inserting a node at the beginning of LinkedList (location 1)
+        csll.insertCSLL(2, 8); // inserting a node at the beginning of LinkedList (location 8)
         System.out.println(csll.head.value);
-        System.out.println(csll.head.next.value);
     }
 }
