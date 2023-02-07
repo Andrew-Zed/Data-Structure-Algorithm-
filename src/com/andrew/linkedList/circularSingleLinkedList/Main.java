eb770bb91e556c2c56802f0b6f4084a62b10bc75
Andrew-Zed <andrew.awadyke@gmail.com>
2023-02-07 16:30:49 +0100

    deletion of node in linkedList implemented

--- src/com/andrew/linkedList/circularSingleLinkedList/Main.java
+++ src/com/andrew/linkedList/circularSingleLinkedList/Main.java
@@ -3,13 +3,18 @@ package com.andrew.linkedList.circularSingleLinkedList;
 public class Main {
     public static void main(String[] args) {
         CircularSinglyLinkedList csll = new CircularSinglyLinkedList();
-        csll.createCSLL(5);
+//        csll.createCSLL(5);
         csll.insertCSLL(4,0); // inserting a node at the beginning of LinkedList (location 0)
         csll.insertCSLL(7, 1); // inserting a node at the beginning of LinkedList (location 1)
-        csll.insertCSLL(2, 8); // inserting a node at the beginning of LinkedList (location 8)
-        System.out.println("head -> " +csll.head.value);
+        csll.insertCSLL(2, 2); // inserting a node at the beginning of LinkedList (location 2)
+        csll.insertCSLL(8, 3); // inserting a node in the LinkedList (location 3)
+        csll.insertCSLL(6, 8); // inserting a node in the LinkedList (location 8)
+
+
+        System.out.println("head -> " + csll.head.value);
         System.out.println("================================");
         csll.traversalCSLL();
-        csll.searchNode(15);
+        csll.deleteNode(1 );
+        csll.traversalCSLL();
     }
 }
