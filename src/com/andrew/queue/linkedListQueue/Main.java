456b5223211d5f1b5ee0833bc0c0bd1215d574b3
Andrew-Zed <andrew.awadyke@gmail.com>
2023-02-18 21:15:45 +0100

    deQueue(), peek() and deleteQueue() implemented

--- src/com/andrew/queue/linkedListQueue/Main.java
+++ src/com/andrew/queue/linkedListQueue/Main.java
@@ -6,8 +6,19 @@ class Main {
         newQLL.enQueue(10);
         newQLL.enQueue(20);
         newQLL.enQueue(30);
-        boolean result = newQLL.isEmpty();
+        newQLL.enQueue(40);
+        int result = newQLL.deQueue();
         System.out.println(result);
+        int result2 = newQLL.deQueue();
+        System.out.println(result2);
+        System.out.println("==========================");
+
+        int result3 = newQLL.peek();
+        System.out.println(result3);
+        int result4 = newQLL.peek();
+        System.out.println(result4);
+
+        newQLL.deleteQueue();
 
     }
 }
