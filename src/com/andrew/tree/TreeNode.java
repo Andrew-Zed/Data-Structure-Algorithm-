a54a2ca65acd5d765cfa880d5facf8851f80aced
Andrew-Zed <andrew.awadyke@gmail.com>
2023-02-21 00:23:46 +0100

    basic tree dataStructure implemented

--- src/com/andrew/tree/TreeNode.java
+++ src/com/andrew/tree/TreeNode.java
@@ -1,4 +1,27 @@
 package com.andrew.tree;
 
-public class TreeNode {
+import java.util.ArrayList;
+
+class TreeNode {
+    String data;
+    ArrayList<TreeNode> children;
+
+     TreeNode(String data) {
+        this.data = data;
+        this.children = new ArrayList<>();
+    }
+
+    void addChild(TreeNode node) {
+         this.children.add(node);
+    }
+
+    String print(int level) {
+         String ret;
+         ret = " ".repeat(level) +  data + "\n";
+        for (TreeNode node: this.children) {
+            ret += node.print(level + 1);
+        }
+        return ret;
+    }
+
 }
