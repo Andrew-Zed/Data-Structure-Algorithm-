b3d628141ee81f7e56a26004e3d79de74150f552
Andrew-Zed <andrew.awadyke@gmail.com>
2023-02-21 18:43:44 +0100

    PreOrder Traversal using linkedList implemented

--- src/com/andrew/tree/binaryTree/linkedListBinaryTree/BinaryTreeLL.java
+++ src/com/andrew/tree/binaryTree/linkedListBinaryTree/BinaryTreeLL.java
@@ -6,4 +6,14 @@ class BinaryTreeLL {
     public BinaryTreeLL() {
         this.root = null;
     }
+
+    // PreOrder Traversal
+    void preOrder(BinaryNode node) {
+        if (node == null) {
+            return;
+        }
+        System.out.print(node.value + " ");
+        preOrder(node.left);
+        preOrder(node.right);
+    }
 }
