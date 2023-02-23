a6d49df51f3d5c63484e4c3a5f2d69702707da3d
Andrew-Zed <andrew.awadyke@gmail.com>
2023-02-23 23:42:51 +0100

    insert method in BT implemented

--- src/com/andrew/tree/binaryTree/arrayBinaryTree/Main_BT_Array.java
+++ src/com/andrew/tree/binaryTree/arrayBinaryTree/Main_BT_Array.java
@@ -1,8 +1,16 @@
 package com.andrew.tree.binaryTree.arrayBinaryTree;
 
-public class Main_BT_Array {
+class Main_BT_Array {
     public static void main(String[] args) {
         BinaryTree newBinaryTree = new BinaryTree(7);
+        newBinaryTree.insert("N1");
+        newBinaryTree.insert("N2");
+        newBinaryTree.insert("N13");
+        newBinaryTree.insert("N4");
+        newBinaryTree.insert("N5");
+        newBinaryTree.insert("N6");
+        newBinaryTree.insert("N7");
+        newBinaryTree.insert("N8");
 
     }
 }
