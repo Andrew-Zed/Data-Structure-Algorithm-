af168d6b7f9a94c8a554a1cb2985777c0bd17d41
Andrew-Zed <andrew.awadyke@gmail.com>
2023-02-23 21:53:15 +0100

    Blank tree of array type created

---
+++ src/com/andrew/tree/binaryTree/arrayBinaryTree/Main_BT_Array.java
@@ -0,0 +1,8 @@
+package com.andrew.tree.binaryTree.arrayBinaryTree;
+
+public class Main_BT_Array {
+    public static void main(String[] args) {
+        BinaryTree newBinaryTree = new BinaryTree(7);
+
+    }
+}
